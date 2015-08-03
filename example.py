9435003c3b9fb17aface297dedb5fd9e68437a8a
wuaalb <merlijn.blaauw@gmail.com>
2015-08-03 18:21:33 +0200

    Added SampleBernoulli Layer

--- example.py
+++ example.py
@@ -11,7 +11,7 @@ from keras.optimizers import SGD
 from keras_extensions.logging import Logger, redirect_std_streams
 from keras_extensions.models import SingleLayerUnsupervised
 from keras_extensions.rbm import GBRBM
-import keras_extensions.rbm
+from keras_extensions.layers import SampleBernoulli
 from keras_extensions.callbacks import MomentumScheduler, UnsupervisedLoss1Logger, UnsupervisedLoss2Logger
 from keras_extensions.initializers import glorot_uniform_sigm
 from keras_extensions.preprocessing import standardize
@@ -25,22 +25,26 @@ input_dim = 100
 hidden_dim = 200
 batch_size = 10
 nb_epoch = 10
-lr = 0.0001
-momentum_schedule = [(0, 0.5), (5, 0.9)]        # start momentum at 0.5, then 0.9 after 5 epochs
+lr = 0.0001  # small learning rate for GB-RBM
+momentum_schedule = [(0, 0.5), (5, 0.9)]  # start momentum at 0.5, then 0.9 after 5 epochs
 
 # seed random number generators
 np.random.seed(1234)
 GBRBM.srng = RandomStreams(np.random.randint(2**30))
 
-# load dataset
+# generate dummy dataset
 nframes = 10000
 dataset = np.random.normal(loc=np.zeros(input_dim), scale=np.ones(input_dim), size=(nframes, input_dim))
 
+# standardize (in this case superfluous)
 dataset, mean, stddev = standardize(dataset)
 
-X_train = dataset[:-1000, :]    # all but last 1000 samples for training
-X_test  = dataset[-1000:, :]    # last 1000 samples for testing
-X_trainsub = dataset[:1000, :]  # subset of training data with same number of samples as testset
+# split into train and test portion
+ntest   = 1000
+X_train = dataset[:-ntest :]     # all but last 1000 samples for training
+X_test  = dataset[-ntest:, :]    # last 1000 samples for testing
+X_trainsub = dataset[:ntest, :]  # subset of training data with same number of samples as testset
+assert nframes-ntest >= ntest, 'Train set should be at least size of test set!'
 
 # create model
 print('Creating training model...')
@@ -90,7 +94,7 @@ rbm.load_weights('example.hdf5')
 # generate hidden features from input data
 print('Creating inference model...')
 h_given_x = rbm.get_h_given_x_layer()
-inference_model = Sequential([h_given_x])
+inference_model = Sequential([h_given_x, SampleBernoulli(mode='maximum_likelihood')])
 
 print('Compiling Theano graph...')
 inference_model.compile(opt, loss='mean_squared_error') # XXX: optimizer and loss are not used!
@@ -98,6 +102,6 @@ inference_model.compile(opt, loss='mean_squared_error') # XXX: optimizer and los
 print('Doing inference...')
 h = inference_model.predict(dataset)
 
-print(np.round(h))
+print(h)
 
 print('Done!')
---
+++ keras_extensions/layers.py
@@ -0,0 +1,33 @@
+import numpy as np
+import theano.tensor as T
+from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
+
+from keras.layers.core import Layer
+
+class SampleBernoulli(Layer):
+    """
+    Layer which samples a Bernoulli distribution whose statistics (means, 'p') are given 
+    as inputs to the layer.
+
+    :param mode:    'maximum_likelihood' for maximum likelihood sample, 
+                    'random' for random sample.
+    """
+    def __init__(self, mode='maximum_likelihood'):
+        super(SampleBernoulli, self).__init__()
+        self.mode = mode
+        self.srng = RandomStreams(seed=np.random.randint(10e6))
+
+    def get_output(self, train=False):
+        p = self.get_input(train)
+        if self.mode.lower() == 'maximum_likelihood':
+            # draw maximum likelihood sample from Bernoulli distribution
+            #    x* = argmax_x p(x) = 1         if p(x=1) >= 0.5
+            #                         0         otherwise
+            return T.round(p, mode='half_away_from_zero')
+        elif self.mode.lower() == 'random':
+            # draw random sample from Bernoulli distribution
+            #    x* = x ~ p(x) = 1              if p(x=1) > uniform(0, 1)
+            #                    0              otherwise
+            return self.srng.binomial(size=p.shape, n=1, p=p, dtype=theano.config.floatX)
+        else:
+            raise NotImplementedError('Unknown sample mode!')
