c211599494badb8ad9e06137a5d8e781dafde0ee
Amos Elberg <amos.elberg@me.com>
2015-12-18 10:24:40 -0500

    Changed defaults on binomial

--- keras_extensions/backend.py
+++ keras_extensions/backend.py
@@ -2,7 +2,7 @@ from keras import backend as K
 import numpy as np
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 
-def random_binomial(shape, n=0.0, p=1.0, dtype=K.floatx(), seed=None):
+def random_binomial(shape, n=0, p=0.5, dtype=K.floatx(), seed=None):
     if seed is None:
         seed = np.random.randint(10e6)
     rng = RandomStreams(seed=seed)
